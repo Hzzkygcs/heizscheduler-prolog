3aea87f4a0d02f9f5aa8481cae39501deca48340
Immanuel01 <immanuel01@ui.ac.id>
2023-04-10 17:25:43 +0700

    refactor(prolog.pl): renamed har(D, H, M) to be D:H:M

--- prolog.pl
+++ prolog.pl
@@ -1,12 +1,14 @@
 
 % {%begin ignore%}
-available(hari(1, 12, 10), hari(1, 14, 15)).
-available(hari(2, 12, 10), hari(2, 14, 15)).
-available(hari(3, 12, 10), hari(3, 14, 15)).
+
+available(1:12:10, 1:14:15).
+available(2:12:10, 2:14:15).
+available(3:12:10, 3:14:15).
+
 % {%end ignore%}
 % {{available_definitions}}
 
 
-time(Hari, Jam, Tanggal) :- available(hari(Hari, Jam, Tanggal), hari(_, _, _)).
-time(Hari, Jam, Tanggal) :- available(hari(_, _, _), hari(Hari, Jam, Tanggal)).
+time(Hari, Jam, Tanggal) :- available(Hari:Jam:Tanggal, _:_:_).
+time(Hari, Jam, Tanggal) :- available(_:_:_, Hari:Jam:Tanggal).
 
--- tests/TestTime.py
+++ tests/TestTime.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from HzzProlog.ChainEquality import apply_to_defined_variables, ChainedEquality, equality
 from HzzProlog.HzzProlog import HzzProlog
-from tests.definitions import available, time, hari, MAIN_PROLOG_FILE_IO, X, Y, Z
+from tests.definitions import available, time, hari, MAIN_PROLOG_FILE_IO, X, Y, Z, time_point
 
 
 class TestTime(TestCase):
@@ -11,8 +11,8 @@ class TestTime(TestCase):
 
     def test_time__should_return_list_of_endpoints_correctly(self):
         self.prolog.add_facts("available_definitions", [
-            available(hari(0, 1, 2), hari(3, 4, 5)),
-            available(hari(6, 10, 10), hari(6, 23, 59)),
+            available(time_point(0, 1, 2), time_point(3, 4, 5)),
+            available(time_point(6, 10, 10), time_point(6, 23, 59)),
         ])
         result = self.prolog.query(time(X, Y, Z))
         expected = [
