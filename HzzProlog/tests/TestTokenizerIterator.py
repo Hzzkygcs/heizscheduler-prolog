eb470ba3234a19c70e47797baf2b5ac290727820
Immanuel01 <immanuel01@ui.ac.id>
2023-04-22 01:10:46 +0700

    refactor(tests): remove testing dependencies on self.include_space
    The purpose of this refactoring is to remove include_space property entirely, make it passed as parameter instead

--- HzzProlog/tests/TestTokenizerIterator.py
+++ HzzProlog/tests/TestTokenizerIterator.py
@@ -9,41 +9,47 @@ class TestTokenizerIterator(TestCase):
 
     def test_tokenize__should_skip_whitespaces_if_told_so(self):
         result = self._instantiate("123 456  789   abc")
-        result.set_include_space(False)
-        self.assertEqual("123", next(result))
-        self.assertEqual("456", next(result))
-        self.assertEqual("789", next(result))
-        self.assertEqual("abc", next(result))
+        include_space = False
+        self.assertEqual("123", result.next(include_space=include_space))
+        self.assertEqual("456", result.next(include_space=include_space))
+        self.assertEqual("789", result.next(include_space=include_space))
+        self.assertEqual("abc", result.next(include_space=include_space))
 
     def test_tokenize__should_include_whitespaces_if_told_so(self):
         result = self._instantiate("123 456  789   abc")
-        result.set_include_space(True)
-        self.assertEqual("123", next(result))
-        self.assertEqual(" ", next(result))
-        self.assertEqual("456", next(result))
-        self.assertEqual(" "*2, next(result))
-        self.assertEqual("789", next(result))
-        self.assertEqual(" "*3, next(result))
-        self.assertEqual("abc", next(result))
+        def do_next():
+            return result.next(include_space=True)
+
+        self.assertEqual("123", do_next())
+        self.assertEqual(" ", do_next())
+        self.assertEqual("456", do_next())
+        self.assertEqual(" "*2, do_next())
+        self.assertEqual("789", do_next())
+        self.assertEqual(" "*3, do_next())
+        self.assertEqual("abc", do_next())
 
     def test_tokenize__should_include_whitespaces_if_told_so_even_if_in_the_middle_of_iterating(self):
         result = self._instantiate("12 34 56 78 9a bc de fg hi")
-        result.set_include_space(False)
-        self.assertEqual("12", next(result))
-        self.assertEqual("34", next(result))
-        self.assertEqual("56", next(result))
+        include_space = False
+        def do_next():
+            return result.next(include_space=include_space)
 
-        result.set_include_space(True)
-        self.assertEqual(" ", next(result))
-        self.assertEqual("78", next(result))
-        self.assertEqual(" ", next(result))
-        self.assertEqual("9a", next(result))
+        include_space = False
+        self.assertEqual("12", do_next())
+        self.assertEqual("34", do_next())
+        self.assertEqual("56", do_next())
 
-        result.set_include_space(False)
-        self.assertEqual("bc", next(result))
-        self.assertEqual("de", next(result))
-        self.assertEqual("fg", next(result))
+        include_space = True
+        self.assertEqual(" ",  do_next())
+        self.assertEqual("78", do_next())
+        self.assertEqual(" ",  do_next())
+        self.assertEqual("9a", do_next())
 
-        result.set_include_space(True)
-        self.assertEqual(" ", next(result))
-        self.assertEqual("hi", next(result))
+        include_space = False
+        self.assertEqual("bc", do_next())
+        self.assertEqual("de", do_next())
+        self.assertEqual("fg", do_next())
+
+        include_space = True
+        self.assertEqual(" ",  do_next())
+        self.assertEqual("hi", do_next())
