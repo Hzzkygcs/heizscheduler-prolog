2e5b513aa99b9a5a5cb03bafd7fa3fe1588d9c6f
Immanuel01 <immanuel01@ui.ac.id>
2023-04-22 01:36:46 +0700

    refactor(test): set dont_care to a randomized atom

--- definitions/variables.py
+++ definitions/variables.py
@@ -1,7 +1,10 @@
 from HzzProlog.PrologCallable import define_variable
+from random import choice
 
 X = define_variable("X")
 Y = define_variable("Y")
 Z = define_variable("Z")
 Result = define_variable("Result")
-dont_care = 0  # arbitrary value
+
+# randomized to proof that the value is truly neglegible when this variable is used
+dont_care = choice(["dontcare", "dont_care", "idontcare", "whatever", "letItBeAnything"])
