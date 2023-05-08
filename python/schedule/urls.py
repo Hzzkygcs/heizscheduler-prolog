e63f10481159215fe637f1758c018c96b1648f70
Tara Mazaya Lababan <tara.mazaya@ui.ac.id>
2023-05-09 01:32:28 +0700

    refactor: add button scheduled events

--- python/schedule/urls.py
+++ python/schedule/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('events/create', EventCreate.as_view(), name='event_create'),
     path('events/edit/<int:event_id>', EventEdit.as_view(), name='event_edit'),
     path('events/<int:event_id>', EventDetail.as_view(), name='event_create'),
+    path('scheduled-events', EventsView.as_view(), name='scheduled_event_list'),
     path('booking/<int:event_id>', BookingDetail.as_view(), name='event_create'),
     path('labels', LabelsView.as_view(), name='label_list'),
     path('', HomePageView.as_view(), name='homepage'),
